c57b6656f20b3cf713b0db33dc73af5ff009682c
japeart <lordwinter@protonmail.com>
2019-05-21 19:36:46 +0100

    msg board test

---
+++ msg/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Message Board</title>
+  </head>
+  <body>
+    <div>
+      <?php phpinfo(); ?>
+    </div>
+
+  </body>
+</html>
