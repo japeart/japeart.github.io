cf61f5b9d1214686cb878e65dd384f73dd811a0f
japeart <lordwinter@protonmail.com>
2019-09-03 17:31:24 +0100

    JQuery test

---
+++ jquery/scripts/scripts.js
@@ -0,0 +1,56 @@
+// just some example jQuery calls
+
+// set up handlers
+document.getElementById('idButton').addEventListener("click", getPostById);
+document.getElementById('userIdButton').addEventListener("click", getUserById);
+
+function getPostById() {
+  let number = document.getElementById('inputId').value;
+  // validate input - number between 1 and 100
+  let pattern = /^[^a-z]+$/i;
+  if(pattern.test(number) && number < 101 && number > 0) {
+    $.getJSON("https://jsonplaceholder.typicode.com/posts/", { id : number}, function(result){
+      $('#results').html(getResultString(result[0]));
+    });
+  } else {
+    alert('invalid input value');
+  }
+  document.getElementById('inputId').value = "";
+}
+
+function getUserById() {
+  console.log('getUserById called');
+  let number = document.getElementById('inputUserId').value;
+  // validate input - number between 1 and 100
+  let pattern = /^[^a-z]+$/i;
+  if(pattern.test(number) && number < 101 && number > 0) {
+    $.getJSON("https://jsonplaceholder.typicode.com/posts/", { userId : number}, function(result){
+      // concatenate result string
+      console.log(JSON.stringify(result[0]));
+      let resultString = "";
+      for(let i = 0; i < result.length; i++) {
+        resultString += getResultString(result[i]);
+      }
+      $('#results').html(resultString);
+    });
+  } else {
+    alert('invalid input value');
+  }
+  document.getElementById('inputUserId').value = "";
+}
+
+// fill the results div
+function getResultString(arr) {
+  let userId = arr.userId;
+  let id = arr.id;
+  let title = arr.title;
+  let body = arr.body;
+
+  return `
+  <div class='entry'>
+    <h3>User Id:</h3>${userId}
+    <h3>Id:</h3>${id}
+    <h3>Title:</h3>${title}
+    <h3>Body:</h3>${body}
+  </div>`;
+}
